b3eb106e993b35504de142bdaac0d28501d0b2c5
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2024-02-22 23:19:47 +0200

    [feat]: core serialization restructuring

--- test_media_post_api/serializers.py
+++ test_media_post_api/serializers.py
@@ -1,9 +1,26 @@
 from rest_framework import serializers
-from .models import Post
+from .models import Post, Comment, Drafts, User
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User,
+        fields = ['username','email','password', 'created_at', 'updated_at']
 
-class MediaSerializer(serializers.ModelSerializer):
+class PostSerializer(serializers.ModelSerializer):
+    user = UserSerializer(required=True)  # Allow the user field to be optional
     class Meta:
         model = Post
-        fields = '__all__'
+        fields = ['title','description','scheduled_datetime','likes','shares','user','draft']
+
+class CommentSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    post = PostSerializer()
+    class Meta:
+        model = Comment
+        fields = ['user','post','comment','created_at','updated_at']
         
+class DraftsSerializer(serializers.ModelSerializer):
+    post = PostSerializer()
+    class Meta:
+        model = Drafts
+        fields = ['post','created_at','updated_at']
