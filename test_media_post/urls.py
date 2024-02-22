b479088d4e04525c1aa167630a3a4e6d16da5874
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2024-02-22 23:18:45 +0200

    [feat]: core referencing the api urls

--- test_media_post/urls.py
+++ test_media_post/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from test_media_post_api import urls as api_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('media-post/', include(api_urls)),
 ]
