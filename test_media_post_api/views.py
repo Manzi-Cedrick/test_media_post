d304b69d6725d118f5c45d048f2d66f42c03c384
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2024-02-22 23:20:12 +0200

    [feat]: core referencing initials --addition

--- test_media_post_api/views.py
+++ test_media_post_api/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Post
+from .serializers import PostSerializer
+from rest_framework import permissions
+from django.contrib.auth.models import User
 
-# Create your views here.
+class PostList(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    def get(self, request):
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK )
+
+    def post(self, request, *args, **kwargs):
+        data = {
+            'title': request.data.get('title'),
+            'description': request.data.get('description'),
+            'scheduled_datetime': request.data.get('scheduled_datetime'),
+            'likes': request.data.get('likes'),
+            'shares': request.data.get('shares'),
+            'comments': request.data.get('comments'),
+            'draft': request.data.get('draft'),
+            "user": {
+                "id": 1
+            }
+        }
+        serializer = PostSerializer(data=data)
+        
+        if serializer.is_valid(raise_exception=True):
+            post_saved = serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
