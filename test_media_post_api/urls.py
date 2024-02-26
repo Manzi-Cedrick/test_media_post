6118de26f0cc5f4ae22427d14372e98ea48e98ae
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2024-02-26 21:51:31 +0200

    [feat]: detailed path configuration --addition

--- test_media_post_api/urls.py
+++ test_media_post_api/urls.py
@@ -5,11 +5,21 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 from .views import (
-    PostList
+    PostList,
+    PostDetail,
+    CommentList,
+    CommentDetail,
+    DraftDetail,
+    DraftList
 )
 
 urlpatterns = [
     path('api/posts', PostList.as_view()),
+    path('api/post/<uuid:pk>', PostDetail.as_view()),
+    path('api/comments', CommentList.as_view()),
+    path('api/post/comment/<uuid:pk>', CommentDetail.as_view()),
+    path('api/draft',DraftList.as_view()),
+    path('api/draft/<uuid:pk>', DraftDetail.as_view()),
     path('token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
 ]
